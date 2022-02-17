bc818c40bb4c17465ea24348402775dc119f08c5
Umar Farouk Yusuf <umaryusuf11@gmail.com>
2022-02-17 01:43:14 +0000

    Created tests for LoadConfig

--- src/test/java/Structure/LoadConfigTest.java
+++ src/test/java/Structure/LoadConfigTest.java
@@ -13,14 +13,31 @@ class LoadConfigTest {
 
     LoadConfig loadConfig = new LoadConfig();
 
+    Scanner mockScanner() {
+        Scanner scanner = mock(Scanner.class);
+        // has two lines then stops, to expand just add more true returns
+        when(scanner.hasNextLine()).thenReturn(true).thenReturn(true).thenReturn(false);
+        when(scanner.nextLine()).thenReturn("Line 1").thenReturn("Line 2");
+
+        return scanner;
+    }
+
     @Test
     void populatePropertyData() {
-        fail("Not yet implemented"); //Remove
+        loadConfig.setPropertyReader(mockScanner());
+        loadConfig.populatePropertyData();
+
+        assertEquals(2, loadConfig.propertyData.size());
     }
 
 
     @Test
     void getConfig() {
-        fail("Not yet implemented"); //Remove
+        loadConfig.setPropertyReader(mockScanner());
+        loadConfig.populatePropertyData();
+
+        assertEquals("Line 1", loadConfig.getConfig().get(0));
+        assertEquals("Line 2", loadConfig.getConfig().get(1));
+
     }
 }
